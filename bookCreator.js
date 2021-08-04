c353d304f868638add722a35a0298f10d80a43a3
SNIKRO <snikr@bk.ru>
2021-08-04 11:36:08 +0600

    Create repositories, create book Creator to create randoms books from faker

---
+++ bookCreator.js
@@ -0,0 +1,20 @@
+let faker = require("faker");
+let fileSave = require("fs");
+faker.locale = "ru";
+function createBooks(numbers){
+    let books = [];
+
+    for(let i = 0; i < numbers; i++){
+        books.push({
+            authorName: faker.name.findName(),
+            bookName: faker.lorem.word(),
+            desription: faker.lorem.paragraph(),
+            yearOfPublishing: faker.date.past()
+            
+        })
+    }
+   return fileSave.writeFile("books.json", JSON.stringify(books, null, 1), function(err, result){
+       if(err) console.log("error", err);
+   }); 
+}
+
